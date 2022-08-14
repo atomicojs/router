a4a9dc1c68ee09a4091634b032d9baa630977df6
UpperCod <uppercod.git@gmail.com>
2022-08-14 14:06:38 -0400

    example

--- example/app.tsx
+++ example/app.tsx
@@ -1,45 +1,83 @@
-import { c, render } from "atomico";
 import { RouterSwitch, RouterCase } from "../src/router";
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const Example = c(() => <host>welcome!</host>);
+const request = (path) =>
+  fetch(`https://pokeapi.co/api/v2/pokemon${path}`).then((res) => res.json());
 
-customElements.define("e-l", Example);
+const thumbnail = (id) => (
+  <img
+    width={96}
+    height={96}
+    loading="lazy"
+    class="thumbnail"
+    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+  />
+);
 
-render(
+const loading = () => (
+  <div class="loading">
+    <h1>Loading</h1>
+  </div>
+);
+
+const delay = () => new Promise((resolve) => setTimeout(resolve, 500));
+
+const vdom = (
   <host>
     <RouterSwitch>
-      <header class="header">
-        <a href="/">home</a>
-        <a href="/user">user</a>
-        <a href="/edit">edit</a>
-        <a href="/config">config</a>
-      </header>
-      <RouterCase path="/" element={Example}></RouterCase>
       <RouterCase
-        path="/{folder}"
+        path="/"
         memo
-        load={async function* (params) {
-          yield <h1>Loading...</h1>;
-
-          await delay(1000);
-
+        load={async function* () {
+          yield loading;
+          const { results } = await request("?limit=252");
           return (
-            <h1>
-              welcome! ({JSON.stringify(params)}) <a href="/">to home</a>
-            </h1>
+            <div class="grid">
+              {results.map(({ name }, id) => (
+                <a class="card" href={`/${id + 1}`}>
+                  {thumbnail(id + 1)}
+                  <span>{name}</span>
+                </a>
+              ))}
+            </div>
           );
         }}
-      ></RouterCase>
-      <div class="router-view" slot="home">
-        <h1>
-          welcome!
-          <a href="/config">[to config]</a>
-          <a href="/users">[to users]</a>
-          <a href="/out">[to out]</a>
-        </h1>
-      </div>
+      />
+      <RouterCase
+        path="/{id}"
+        memo
+        load={async function* ({ id }) {
+          yield loading;
+          await delay();
+          const { abilities, name } = await request(`/${id}`);
+          return (
+            <div class="single">
+              <div class="card">
+                {thumbnail(id)}
+                <h1>{name}</h1>
+                <h2>Abilities</h2>
+                <ul>
+                  {abilities.map(({ ability }) => (
+                    <li>{ability.name}</li>
+                  ))}
+                </ul>
+                <div class="pagination">
+                  <a class="button" href={`/${Number(id) + 1}`}>
+                    Prev
+                  </a>
+                  <a class="button" href="/">
+                    Home
+                  </a>
+                  <a class="button" href={`/${Number(id) - 1}`}>
+                    Next
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      />
     </RouterSwitch>
-  </host>,
-  document.body
+  </host>
 );
+
+vdom.render(document.body);
